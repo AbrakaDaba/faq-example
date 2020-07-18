578a855c3a24f0eafc0ad65fddeb4de38664fd4b
David Damnjanovic <36664588+AbrakaDaba@users.noreply.github.com>
2020-07-18 20:15:55 +0200

    Update rv-iframe.js

--- rv-iframe.js
+++ rv-iframe.js
@@ -8,6 +8,7 @@
     iframe.setAttribute("src", iframeSource);
     iframe.setAttribute("style", "border: 0px; margin: 0px auto; display: block;");
     iframe.setAttribute("id", "rv_iframe");
+     iframe.setAttribute("onload", "setIframeHeight(this.id)");
     // iframe.style.width = 450 + "px";
     // iframe.style.height = 400 + "px";
   iframeBox.appendChild(iframe);
