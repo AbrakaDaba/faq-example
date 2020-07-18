ec4e96c6b9fb0121cfe576dfa59bfd0d5bb1ecaf
David Damnjanovic <36664588+AbrakaDaba@users.noreply.github.com>
2020-07-18 20:35:32 +0200

    Update rv-iframe.js

--- rv-iframe.js
+++ rv-iframe.js
@@ -1,16 +1,5 @@
 (function () {
 
-    var iframeSource = "https://abrakadaba.github.io/faq-example/";
-
-    var iframe = document.createElement("iframe");
-    var iframeBox = document.getElementById("rv-iframe-box"); 
-    iframe.setAttribute("src", iframeSource);
-    iframe.setAttribute("style", "border: 0px; margin: 0px auto; display: block;visibility:hidden;");
-    iframe.setAttribute("id", "rv_iframe");
-     iframe.setAttribute("onload", "setIframeHeight(this.id)"); 
-    iframeBox.appendChild(iframe);
-
-    
      function getDocHeight(doc) {
         doc = doc || document;
         // stackoverflow.com/questions/1145850/
@@ -30,6 +19,20 @@
         ifrm.style.height = getDocHeight( doc ) + 4 + "px";
         ifrm.style.visibility = 'visible';
     }
+    
+    
+    var iframeSource = "https://abrakadaba.github.io/faq-example/";
+
+    var iframe = document.createElement("iframe");
+    var iframeBox = document.getElementById("rv-iframe-box"); 
+    iframe.setAttribute("src", iframeSource);
+    iframe.setAttribute("style", "border: 0px; margin: 0px auto; display: block;visibility:hidden;");
+    iframe.setAttribute("id", "rv_iframe");
+     iframe.setAttribute("onload", "setIframeHeight(this.id)"); 
+    iframeBox.appendChild(iframe);
+
+    
+    
     
     
 })();
